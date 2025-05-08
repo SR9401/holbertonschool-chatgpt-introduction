81d768095eea454e0e453e78be88984bda5bc70f
SR9401 <shakibrojas@icloud.com>
2025-05-08 22:47:16 +0200

    ppp

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,19 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n-=1
     return result
 
-if len(sys.argv) != 2:
-    print("Usage: ./factorial.py <non-negative integer>")
-    sys.exit(1)
-
-try:
-    n = int(sys.argv[1])
-    if n < 0:
-        raise ValueError
-except ValueError:
-    print("Please provide a non-negative integer.")
-    sys.exit(1)
-
-print(factorial(n))
+f = factorial(int(sys.argv[1]))
+print(f)
